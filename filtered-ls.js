dd2d727c3908d19f39362cbef08a64ddab0b7579
gr8geek <pratyush9000@gmail.com>
2020-08-24 13:55:40 +0530

    made second commit

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,10 +1,9 @@
+const path = require('path')
 const fs = require('fs')
 const myFunction = ()=>{
     fs.readdir(process.argv[2],(err,data)=>{
-       
             data.forEach(file =>{
-                var str = file.split('.')
-                if(str[1]==process.argv[3]){
+                if(path.extname(file)=='.'+process.argv[3]){
                     console.log(file);
                 }
         })
