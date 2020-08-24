aa28c8a9919e91c83daa62c94ee59feba0e0be5c
gr8geek <pratyush9000@gmail.com>
2020-08-24 21:59:18 +0530

    Commit done for http fetcher

--- http-collect.js
+++ http-collect.js
@@ -1,13 +1,16 @@
 const http = require('http')
 http.get(process.argv[2],(res)=>{
+    var string1=""
     res.setEncoding('utf-8')
     res.on('err',(err)=>{
         console.log(err)
         return;
     })
     res.on('data',(data)=>{
-        console.log(data.length())
-        console.log(data)
-        return;
+        string1+=data
+    })
+    res.on('end',()=>{
+        console.log(string1.length)
+        console.log(string1)
     })
 })
